199895ccf53ac3e6c2d87663aff0e454b77cde43
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-05 13:43:01 +0530

    update prg2 2nd

--- prg2/Program.cs
+++ prg2/Program.cs
@@ -1,39 +1,39 @@
 ﻿        // prime number code for number;
 
-using System;
+// using System;
 
-public class Solution
-{
-    public static void Main(string[] args)
-    {
-        int num = 10;
-        bool isPrime = false;
-        Console.Write($"Prime Numbers from 1 to {num} are :");
+// public class Solution
+// {
+//     public static void Main(string[] args)
+//     {
+//         int num = 10;
+//         bool isPrime = false;
+//         Console.Write($"Prime Numbers from 1 to {num} are :");
 
-        for (int i = 1; i <= num; i++)
-        {
-            isPrime = true;
+//         for (int i = 1; i <= num; i++)
+//         {
+//             isPrime = true;
 
-            if (i <= 1)
-            {
-                continue;
-            }
+//             if (i <= 1)
+//             {
+//                 continue;
+//             }
 
-            for (int j = 2; j <= Math.Sqrt(i); j++)
-            {
-                if (i % j == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime)
-            {
-                Console.Write(i+" ");
-            }
-        }
-    }
-}
+//             for (int j = 2; j <= Math.Sqrt(i); j++)
+//             {
+//                 if (i % j == 0)
+//                 {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+//             if (isPrime)
+//             {
+//                 Console.Write(i+" ");
+//             }
+//         }
+//     }
+// }
 
 
         // prime number code for a given array 
@@ -45,11 +45,12 @@ public class Solution
     public static void Main()
     {
         int[] num = {1, 2, 4, 6, 8, 10, 27, 19};
+        Console.Write($"Prime Numbers from the given array are :");
         foreach (int i in num)
         {
             if (IsPrime(i))
             {
-                Console.WriteLine(i);
+                Console.Write(i+" ");
             }
         }
     }
