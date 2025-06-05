891b9682d218741f4658211e587ff02b8ab0a7d1
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-05 19:44:19 +0530

    prg 46 & 47 added

---
+++ prg46/Program.cs
@@ -0,0 +1,18 @@
+﻿        //Sum of two numbers using Constructors
+
+using System;
+public class Addition
+{
+    public Addition(int a, int b)
+    {
+        int sum = a + b;
+        Console.WriteLine($"sum of {a} and {b} is : {sum}");
+    }
+}
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        Addition obj = new Addition(5, 10);
+    }
+}
---
+++ prg47/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        for (int i = arr.Length-1; i>=0; i--)
+        {
+            Console.Write(arr[i]+" ");
+        }
+    }
+}
