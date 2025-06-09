e57a82b003c3e5d36d2d79a27a91ebdfb8bcbcec
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-09 19:46:44 +0530

    updated prg47

--- prg47/Program.cs
+++ prg47/Program.cs
@@ -1,4 +1,20 @@
-﻿        //Reverse an array using for loop
+﻿//Reverse an array using for loop(not actualling reversing, just printing it in reverse order)
+
+// using System;
+// public class Solution
+// {
+//     public static void Main(string[] args)
+//     {
+//         int[] arr = { 1, 2, 3, 4, 5 };
+//         for (int i = arr.Length - 1; i >= 0; i--)
+//         {
+//             Console.Write(arr[i] + " ");
+//         }
+//     }
+// }
+
+//Reverse an array using for loop(actualling reversing)
+
 
 using System;
 public class Solution
@@ -6,9 +22,16 @@ public class Solution
     public static void Main(string[] args)
     {
         int[] arr = { 1, 2, 3, 4, 5 };
-        for (int i = arr.Length-1; i>=0; i--)
+        int n = arr.Length;
+        for (int i = 0; i < n / 2; i++)
         {
-            Console.Write(arr[i]+" ");
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
         }
+        foreach (int i in arr)
+        {
+            Console.Write(i + " ");
+        }       
     }
 }
