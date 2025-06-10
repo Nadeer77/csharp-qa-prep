aef067053f57a4b5507b431ac4994a57c92eee25
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-10 22:31:48 +0530

    prg50 added

---
+++ prg50/Program.cs
@@ -0,0 +1,40 @@
+﻿        // Const example
+
+using System;
+
+class Vehicle
+{
+    public const int wheels = 4;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("ഒരു വാഹനത്തിന്  " + Vehicle.wheels + " ചക്രങ്ങൾ ഉണ്ട്.");
+    }
+}
+
+
+        // readonly example
+
+using System;
+
+class Car
+{
+    public readonly string model;
+
+    public Car(string carModel)
+    {
+        model = carModel; // Constructor-ൽ assign ചെയ്യുന്നു
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Car c = new Car("Toyota");
+        Console.WriteLine("കാറിന്റെ മോഡൽ: " + c.model);
+    }
+}
