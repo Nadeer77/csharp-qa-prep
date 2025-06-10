6e36ab5588d17ccfc271da0efc04b9fddc1fbaa4
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-10 06:57:15 +0530

    added prg48 &49

---
+++ prg48/Program.cs
@@ -0,0 +1,22 @@
+﻿//Data abstraction example using Abstract class
+
+using System;
+public abstract class Animal
+{
+    public abstract void MakeSound();
+}
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog Barks");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal obj = new Dog();
+        obj.MakeSound();
+    }
+}
---
+++ prg49/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public interface IShape
+{
+    public void Draw();
+}
+public class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a Circle");
+    }
+}
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        IShape obj = new Circle();
+        obj.Draw();
+    }
+}
