6bdf3e326ad35fb218ebfec6e704c414df20a80f
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-10 15:09:28 +0530

    update prg1 using all loops

--- prg1/Program.cs
+++ prg1/Program.cs
@@ -1,4 +1,4 @@
-﻿        //finding the second largest word in a string
+﻿        //finding the second largest word in a stringusing foreach loop
 
 using System;
 
@@ -25,5 +25,68 @@ public class HelloWorld
             }
         }
         Console.WriteLine($"Second Largest word: {secondlargestword}");
+    }
+}
+
+        // finding the second largest word in a string using for loop
+
+using System;
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        string sentence = "my name is nadeer";
+        string[] words = sentence.Split(' ');
+
+        string largest = "";
+        string secondlargest = "";
+        for (int i = 0; i < words.Length; i++)
+        {
+            string word = words[i];
+            if (word.Length >= largest.Length)
+            {
+                secondlargest = largest;
+                largest = word;
+            }
+            else if (word.Length > secondlargest.Length && word != largest)
+            {
+                secondlargest = word;
+            }
+        }
+        Console.WriteLine($"Second Largest word: {secondlargest}");
+    }
+}
+
+        //finding the second largest word in a string using while loop
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string sentence = "my name is nadeer";
+        string[] words = sentence.Split(' ');
+
+        string largest = "";
+        string secondlargest = "";
+
+        int i = 0;
+        while (i < words.Length)
+        {
+            string word = words[i];
+
+            if (word.Length >= largest.Length)
+            {
+                secondlargest = largest;
+                largest = word;
+            }
+            else if (word.Length > secondlargest.Length && word != largest)
+            {
+                secondlargest = word;
+            }
+            i++;
+        }
+        Console.WriteLine($"Second Largest word: {secondlargest}");
     }
 }
