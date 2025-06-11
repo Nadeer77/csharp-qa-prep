59e622026274036112b69402a0d6f1414a1f3e5e
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-11 22:16:18 +0530

    prg51 added

---
+++ prg51/Program.cs
@@ -0,0 +1,31 @@
+﻿        // Multiple Inheritance Example
+
+using System;
+interface ITalk
+{
+    void Talk();
+}
+interface IWalk
+{
+    void Walk();
+}
+public class Person : ITalk, IWalk
+{
+    public void Talk()
+    {
+        Console.WriteLine("Person is Talking");
+    }
+    public void Walk()
+    {
+        Console.WriteLine("Person is Walking");
+    }
+}
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Person p = new Person();
+        p.Talk();
+        p.Walk();
+    }
+}
