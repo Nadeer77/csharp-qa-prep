29a07df90e70dd16b8dd7e2d1c5b9ecb827b6d66
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-12 22:43:30 +0530

    prg54 added

---
+++ prg54/Program.cs
@@ -0,0 +1,38 @@
+﻿        //Hybrid Inheritance example
+
+using System;
+class Person
+{
+    public void ShowDetails()
+    {
+        Console.WriteLine("Person details");
+    }
+}
+class Student : Person
+{
+    public void Study()
+    {
+        Console.WriteLine("Studying");
+    }
+}
+interface ISports
+{
+    void Play();
+}
+class Topper : Student, ISports
+{
+    public void Play()
+    {
+        Console.WriteLine("Playing sports");
+    }
+}
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Topper top = new Topper();
+        top.ShowDetails();
+        top.Study();
+        top.Play();
+    }
+}
