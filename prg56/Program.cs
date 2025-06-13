ee1fa4c63d8f8a1544be82454a0a953e3bb639aa
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-13 18:49:42 +0530

    prg56 added

---
+++ prg56/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        // Leap year logic
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine(year + " is a Leap Year.");
+        }
+        else
+        {
+            Console.WriteLine(year + " is NOT a Leap Year.");
+        }
+    }
+}
