3c9c405cb2d12f3064a4c368547f415b065f865e
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-13 19:36:22 +0530

    prg60 added

---
+++ prg60/Program.cs
@@ -0,0 +1,34 @@
+﻿        //Nested if example
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine("The number is positive.");
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero.");
+        }
+    }
+}
