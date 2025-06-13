c016e76fffdf45988c3ba6ac82a35d3cf3446e38
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-13 19:01:34 +0530

    added prg58

--- prg58/Program.cs
+++ prg58/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿        //Switch Case for Days of the Week
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter a number (1 to 7): ");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("Invalid day number.");
+                break;
+        }
+    }
+}
