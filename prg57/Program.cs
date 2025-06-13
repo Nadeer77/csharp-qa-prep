4c313a0f4c0d00e2bbba749d0dd46842d9e29701
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-13 18:59:22 +0530

    added prg57 & 58

---
+++ prg57/Program.cs
@@ -0,0 +1,31 @@
+﻿        //Switch case for choosing options (1,2,3,...)
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number (1 to 3): ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("You chose One");
+                break;
+
+            case 2:
+                Console.WriteLine("You chose Two");
+                break;
+
+            case 3:
+                Console.WriteLine("You chose Three");
+                break;
+
+            default:
+                Console.WriteLine("Invalid choice");
+                break;
+        }
+    }
+}
