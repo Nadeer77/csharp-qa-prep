9e1065f0e3f215e2f0c3844a713ac6c69d75004c
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-16 21:57:35 +0530

    prg46 updated

--- prg46/Program.cs
+++ prg46/Program.cs
@@ -16,3 +16,26 @@ public class Example
         Addition obj = new Addition(5, 10);
     }
 }
+
+        //Sum of two numbers using Constructors (another way)
+
+using System;
+
+public class Addition
+{
+    private int sum;
+    public Addition(int a , int b)
+    {
+        sum = a+b;
+    }
+    public int GetSum()
+    {
+        return sum;
+    }
+    public static void Main(string[] args)
+    {
+        Addition obj = new Addition(5,10);
+        int result = obj.GetSum();
+        Console.WriteLine($"Sum of 5 and 10 are: {result}");
+    }
+}
