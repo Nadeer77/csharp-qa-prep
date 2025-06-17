b253ff79a999c8f03eb34d38ad435fe5777e2a7b
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-17 20:26:53 +0530

    prg65 added

---
+++ prg65/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Player
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the Player Health: ");
+        int playerHealth = Convert.ToInt32(Console.ReadLine()!);
+        bool isAlive =false;
+        if(playerHealth > 100)
+        {
+            Console.WriteLine("Invalid Health stat");
+            return;
+        }
+        else if(playerHealth > 0 && playerHealth <= 100)
+        {
+            isAlive =true;
+        }
+        else
+        {
+            isAlive = false;
+        }
+        
+        if(isAlive == true)
+        {
+            Console.WriteLine("Player is Alive");
+        }
+        else
+        {
+            Console.WriteLine("Player is Dead");
+        }
+    }
+}
