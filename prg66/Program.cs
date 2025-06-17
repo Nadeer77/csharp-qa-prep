4014d7eb15277564991b1c62d444ee5be45a06d0
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-17 20:49:44 +0530

    prg66 added

---
+++ prg66/Program.cs
@@ -0,0 +1,19 @@
+﻿        //GameDevHQ unity basics- task 1(Variables)
+
+using System;
+
+public class Player
+{
+    public string name = "Nadeer";
+    public  int age =23;
+    public string gender = "male";
+    public string location = "Kasaragod";
+    public static void Main(string[] args)
+    {
+        Player p = new Player();
+        Console.WriteLine(p.name);
+        Console.WriteLine(p.age);
+        Console.WriteLine(p.gender);
+        Console.WriteLine(p.location);
+    }
+}
