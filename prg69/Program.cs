53070bf2660cd549a3e73977add53eff049a33ed
mohammed nadeer <mohammednadeer20@gmail.com>
2025-06-27 22:26:16 +0530

    prg69 added

---
+++ prg69/Program.cs
@@ -0,0 +1,40 @@
+﻿        // TO check given number is Harshad number or not
+
+using System;
+public class Solution
+{
+    public int SumOfTheDigitsOfHarshadNumber(int x)
+    {
+        int sum = 0;
+        int current = x;
+
+        while (current > 0)
+        {
+            sum += current % 10;
+            current /= 10;
+        }
+
+        return x % sum == 0 ? sum : -1;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number to check if it is a Harshad number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Solution solution = new Solution();
+        int result = solution.SumOfTheDigitsOfHarshadNumber(number);
+
+        if (result != -1)
+        {
+            Console.WriteLine($"{number} is a Harshad number.");
+            Console.WriteLine($"Sum of its digits: {result}");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is NOT a Harshad number.");
+        }
+    }
+}
