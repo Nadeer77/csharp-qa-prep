910e8be3d1a6dc90d9337de46729fa0f5a0acf2f
mohammed nadeer <mohammednadeer20@gmail.com>
2025-07-21 22:17:54 +0530

    prg70 added

---
+++ prg70/Program.cs
@@ -0,0 +1,20 @@
+﻿        //Simple Async program
+
+using System;
+using System.Threading.Tasks;
+
+class Solution
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Start");
+        await DoSomethingAsync();
+        Console.WriteLine("End");
+    }
+    static async Task DoSomethingAsync()
+    {
+        Console.WriteLine("Do something...");
+        await Task.Delay(2000);
+        Console.WriteLine("Finished Doing!!");
+    }
+}
