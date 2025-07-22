eb7faeb2293ed9c5657de0466debce896878dd71
mohammed nadeer <mohammednadeer20@gmail.com>
2025-07-22 22:44:26 +0530

    prg73 added

---
+++ prg73/Program.cs
@@ -0,0 +1,27 @@
+﻿        //Simple Struct example in c#
+
+using System;
+struct Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"X = {X}, Y = {Y}");
+    }
+}
+class Solution
+{
+    static void Main()
+    {
+        Point p = new Point(10,20);
+        p.Display();
+    }
+}
