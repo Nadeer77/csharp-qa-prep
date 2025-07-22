463d946f55259ffd276c348b5b9f35bf01631fbc
mohammed nadeer <mohammednadeer20@gmail.com>
2025-07-22 14:07:23 +0530

    prg72 added

---
+++ prg72/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+class Solution
+{
+    static void PrintNumbers()
+    {
+          for(int i=0;i<5;i++)
+          {
+              Console.WriteLine(i);
+              Thread.Sleep(1000);
+          }
+    }
+    static void Main()
+    {
+        Thread thread = new Thread(PrintNumbers);
+        thread.Start();
+        Console.WriteLine("Main Thread Continues");
+    }
+}
