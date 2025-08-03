7c0ca5270413f24d33cabc84072d26c4ff9a1667
mohammed nadeer <mohammednadeer20@gmail.com>
2025-08-03 22:10:35 +0530

    prg74 added

---
+++ prg74/Program.cs
@@ -0,0 +1,24 @@
+﻿        // IEnumertor and IEnumerable example
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+public class MyCollection : IEnumerable
+{
+    private int[] numbers = { 1, 2, 3 };
+    public IEnumerator GetEnumerator()
+    {
+        return numbers.GetEnumerator();  
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        MyCollection collection = new MyCollection();
+        foreach (int number in collection)
+        {
+            Console.WriteLine(number);  // Output: 1 2 3
+        }
+    }
+}
