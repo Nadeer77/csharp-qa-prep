1a485274f99f27f12d21807d54a3117fe030fb3d
mohammed nadeer <mohammednadeer20@gmail.com>
2025-08-03 22:13:36 +0530

    prg75 added

---
+++ prg75/Program.cs
@@ -0,0 +1,27 @@
+﻿        // IEnumertor and IEnumerable example using fruits(string)
+
+using System;
+using System.Collections;
+
+public class Fruits : IEnumerable
+{
+    string[] fruitList = { "Apple", "Banana", "Cherry" };
+
+    public IEnumerator GetEnumerator()
+    {
+        return fruitList.GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Fruits fruits = new Fruits();
+
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);  
+        }
+    }
+}
