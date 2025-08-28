a149272c88d8b10fbdf9cc6fd6471d5a2e74a8b8
mohammed nadeer <mohammednadeer20@gmail.com>
2025-08-28 11:50:06 +0530

    prg88 added

---
+++ prg88/Program.cs
@@ -0,0 +1,28 @@
+﻿        //Linear Search
+
+using System;
+
+public class Search
+{
+    public int Linear(int[] arr, int target)
+    {
+        for(int i=0;i<arr.Length;i++)
+        {
+            if(arr[i]==target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = {1,2,3,4,5,6,7};
+        Search li = new Search();
+        int result = li.Linear(arr,3);
+        Console.WriteLine(result);
+    }
+}
